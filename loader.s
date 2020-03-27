48c5136e9b4d4804f528f9c8e0280b64b979d372
Sunny Batabyal <batabyalsunny@gmail.com>
2020-03-28 03:52:20 +0530

    Constructor calls commented

--- loader.s
+++ loader.s
@@ -9,12 +9,12 @@
 
 .section .text
 .extern kernelMain
-.extern callConstructors
+# .extern callConstructors
 .global loader
 
 loader:
     mov $kernel_stack, %esp
-    call callConstructors
+    # call callConstructors
     push %eax
     push %ebx
     call kernelMain
